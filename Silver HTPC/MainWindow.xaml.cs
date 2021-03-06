7c5fea877283266b6e34e066be4fad4acf3586ca
Kam5678 <kamalmundi31@gmail.com>
2021-03-05 22:17:18 -0700

    Recording Screen, added recorded shows

--- Silver HTPC/MainWindow.xaml.cs
+++ Silver HTPC/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Silver_HTPC
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
