3898d8362638ef48b0bbe72d3e0ff808e74e2eab
Kam5678 <kamalmundi31@gmail.com>
2021-03-07 12:56:13 -0700

    Added Photos/Video page. Runs off its on window when program is ran(This is temporary).

--- Silver HTPC/Page1.xaml.cs
+++ Silver HTPC/Page1.xaml.cs
@@ -16,21 +16,14 @@ using System.Windows.Shapes;
 namespace Silver_HTPC
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Page1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Page1 : Page
     {
-        public MainWindow()
+        public Page1()
         {
             InitializeComponent();
-            test win = new test();
-            win.Show();
-            
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+       
     }
 }
--- Silver HTPC/test.xaml.cs
+++ Silver HTPC/test.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Silver_HTPC
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for test.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class test : Window
     {
-        public MainWindow()
+        public test()
         {
             InitializeComponent();
-            test win = new test();
-            win.Show();
-            
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
