c1382b5019ab89d953be16835c391b81ccbaf03e
Britta Den Hoed <britta.denhoed1@ucalgary.ca>
2021-03-17 20:22:18 -0600

    No longer broken!

--- Silver HTPC/Advanced Settings.xaml.cs
+++ Silver HTPC/Advanced Settings.xaml.cs
@@ -18,9 +18,9 @@ namespace Silver_HTPC
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdvancedSettings : Window
     {
-        public MainWindow()
+        public AdvancedSettings()
         {
             InitializeComponent();
         }
