010d67b7572dee0b0512542fd1fecb92b7b7b695
Sebastian <bbxtnation@gmail.com>
2021-04-11 17:27:06 -0600

    Search: Space and backspace bug fixes

--- Silver HTPC/Search.xaml.cs
+++ Silver HTPC/Search.xaml.cs
@@ -554,7 +554,7 @@ namespace Silver_HTPC
             {
                 foreach (Grid res in Results)
                 {
-                    if (RemoveSpecialCharacters(res.Name.ToLower()).Contains(RemoveSpecialCharacters(searchBox.Text)))
+                    if (RemoveSpecialCharacters(res.Name.ToLower()).Contains(RemoveSpecialCharacters(searchBox.Text)) && !RemoveSpecialCharacters(searchBox.Text).Equals(""))
                     {
                         found = true;
                         stack.Children.Add(res);
@@ -566,7 +566,7 @@ namespace Silver_HTPC
             {
                 foreach (Grid res in Results)
                 {
-                    if (filterVal.Contains(((Button)res.Children[1]).Name) && RemoveSpecialCharacters(res.Name.ToLower()).Contains(RemoveSpecialCharacters(searchBox.Text)))
+                    if (filterVal.Contains(((Button)res.Children[1]).Name) && RemoveSpecialCharacters(res.Name.ToLower()).Contains(RemoveSpecialCharacters(searchBox.Text)) && !RemoveSpecialCharacters(searchBox.Text).Equals(""))
                     {
                         found = true;
                         stack.Children.Add(res);
@@ -781,8 +781,11 @@ namespace Silver_HTPC
                 }
                 else if (btn.Content.Equals("Backspace"))
                 {
-                    searchBox.Text = searchBox.Text.Substring(0, searchBox.Text.Length - 1);
-                    text = text.Substring(0, text.Length - 1);
+                    if (searchBox.Text.Length > 0)
+                    {
+                        searchBox.Text = searchBox.Text.Substring(0, searchBox.Text.Length - 1);
+                        text = text.Substring(0, text.Length - 1);
+                    }
                 }
             } 
             else if (e.Key == Key.Right && btn == ((Grid)keypad.Children[0]).Children[2])
