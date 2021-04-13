912e596e09dee8bf7b1afafbf73300a4d6402850
Aifaz Dhuka <aifaz.dhuka1@ucalgary.ca>
2021-04-13 15:23:39 -0600

    added icons

--- Silver HTPC/MainWindow.xaml.cs
+++ Silver HTPC/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Silver_HTPC
         private static Button[] menuButtonList;
         private static StackPanel[] stackPanelList;
         private DispatcherTimer dispatcherTimer;
-        public static string[,] content=new string[,] { { "Live TV", "Image/tv_icon.png" }, { "Gallery", "Image/gallery_icon.png" }, { "Music", "Image/music_icon.png" }, { "Recordings", "Image/record_icon.png" }, { "Search", "Image/search_icon.jpg" }, { "Netflix", "Image/netflix_icon.png" }, { "Settings", "Image/settings_icon.png" }, { "Notification", "Image/notification_icon.png" }, { "Other Apps", "Image/apps_icon.png" } , {"John Doe","Image/profile_icon.png" } };
+        public static string[,] content=new string[,] { { "Live TV", "Image/tv_icon.png" }, { "Gallery", "Image/gallery_icon.png" }, { "Music", "Image/music_icon.png" }, { "Recordings", "Image/record_icon.png" }, { "Search", "Image/search_icon.png" }, { "Netflix", "Image/netflix_icon.png" }, { "Settings", "Image/settings_icon.png" }, { "Notification", "Image/notification_icon.png" }, { "Other Apps", "Image/apps_icon.png" } , {"John Doe","Image/profile_icon.png" } };
         private static Label selectedLabel;
         private static string[,] profiles;
         private static List<Button> profileBtns = new List<Button>();
@@ -69,8 +69,8 @@ namespace Silver_HTPC
                 }
                 else
                 {
-                    image.Height = 55;
-                    image.Width = 55;
+                    image.Height = 60;
+                    image.Width = 60;
                 }
                 Uri imageUri = new Uri(content[i,1], UriKind.Relative);
                 image.Source = new BitmapImage(imageUri);
@@ -150,7 +150,7 @@ namespace Silver_HTPC
             //{
             selectedLabel = new Label();
                 selectedLabel.Name = "labelSelected";
-                selectedLabel.FontSize = 25;
+                selectedLabel.FontSize = 20;
                 //selectedLabel.Foreground = Brushes.WhiteSmoke;
                 selectedLabel.Content = content[button_index, 0];
                 stackPanelList[button_index].Children.Add(selectedLabel);
